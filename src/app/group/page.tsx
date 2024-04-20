cec3e5855581563c1144cb1d9e34d2cb30abbdce
Nam <namdeveloper.ca@gmail.com>
2024-04-20 17:30:26 +0700

    group

---
+++ src/app/group/page.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from 'next'
+import { RecentGroupList } from './_components/recent-group-list'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+export const metadata: Metadata = {
+  title: 'Recently visited groups',
+}
+
+export default async function GroupsPage() {
+  return (
+    <>
+      <div className='flex flex-col sm:flex-row sm:items-center justify-between gap-2'>
+        <h1 className='font-bold text-2xl flex-1'>
+          <Link href='/groups'>My groups</Link>
+        </h1>
+        <div className='flex gap-2'>
+          {/* <AddGroupByUrlButton reload={reload} /> */}
+          <Button asChild>
+            <Link href='/groups/create'>Create</Link>
+          </Button>
+        </div>
+      </div>
+      <div>Group List</div>
+    </>
+  )
+}
