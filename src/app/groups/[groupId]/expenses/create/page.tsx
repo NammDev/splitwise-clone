f5d0381b7c963c09cbfab04fdc74dd803e836029
Nam <namdeveloper.ca@gmail.com>
2024-04-23 17:30:34 +0700

    layout for expenses form

---
+++ src/app/groups/[groupId]/expenses/create/page.tsx
@@ -0,0 +1,44 @@
+// import { ExpenseForm } from '@/components/expense-form'
+// import { createExpense, getCategories } from '@/lib/api'
+// import { getRuntimeFeatureFlags } from '@/lib/featureFlags'
+import { getCategories } from '@/lib/actions/categories'
+import { createExpense } from '@/lib/actions/expenses'
+import { getCacheGroup } from '@/lib/actions/group'
+import { expenseFormSchema } from '@/lib/schemas'
+import { Metadata } from 'next'
+import { notFound, redirect } from 'next/navigation'
+import { Suspense } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Create expense',
+}
+
+export default async function ExpensePage({
+  params: { groupId },
+}: {
+  params: { groupId: string }
+}) {
+  const group = await getCacheGroup(groupId)
+  if (!group) notFound()
+
+  const categories = await getCategories()
+
+  async function createExpenseAction(values: unknown) {
+    'use server'
+    const expenseFormValues = expenseFormSchema.parse(values)
+    await createExpense(expenseFormValues, groupId)
+    redirect(`/groups/${groupId}`)
+  }
+
+  return (
+    <Suspense>
+      {/* <ExpenseForm
+        group={group}
+        categories={categories}
+        onSubmit={createExpenseAction}
+        runtimeFeatureFlags={await getRuntimeFeatureFlags()}
+      /> */}
+      <h1>Expense Form</h1>
+    </Suspense>
+  )
+}
--- src/lib/actions/expenses.ts
+++ src/lib/actions/expenses.ts
@@ -1,4 +1,8 @@
+import { Expense } from '@prisma/client'
 import { db } from '../db'
+import { randomId } from '../utils'
+import { ExpenseFormValues } from '../schemas'
+import { getGroup } from './group'
 
 export async function getGroupExpenses(groupId: string) {
   return db.expense.findMany({
@@ -9,5 +13,54 @@ export async function getGroupExpenses(groupId: string) {
       category: true,
     },
     orderBy: [{ expenseDate: 'desc' }, { createdAt: 'desc' }],
+  })
+}
+
+export async function createExpense(
+  expenseFormValues: ExpenseFormValues,
+  groupId: string
+): Promise<Expense> {
+  const group = await getGroup(groupId)
+  if (!group) throw new Error(`Invalid group ID: ${groupId}`)
+
+  for (const participant of [
+    expenseFormValues.paidBy,
+    ...expenseFormValues.paidFor.map((p) => p.participant),
+  ]) {
+    if (!group.participants.some((p) => p.id === participant))
+      throw new Error(`Invalid participant ID: ${participant}`)
+  }
+
+  return db.expense.create({
+    data: {
+      id: randomId(),
+      groupId,
+      expenseDate: expenseFormValues.expenseDate,
+      categoryId: expenseFormValues.category,
+      amount: expenseFormValues.amount,
+      title: expenseFormValues.title,
+      paidById: expenseFormValues.paidBy,
+      splitMode: expenseFormValues.splitMode,
+      paidFor: {
+        createMany: {
+          data: expenseFormValues.paidFor.map((paidFor) => ({
+            participantId: paidFor.participant,
+            shares: paidFor.shares,
+          })),
+        },
+      },
+      isReimbursement: expenseFormValues.isReimbursement,
+      documents: {
+        createMany: {
+          data: expenseFormValues.documents.map((doc) => ({
+            id: randomId(),
+            url: doc.url,
+            width: doc.width,
+            height: doc.height,
+          })),
+        },
+      },
+      notes: expenseFormValues.notes,
+    },
   })
 }
